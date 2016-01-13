d983e6bea7ab7935d24e01f613926a5a26053743
xoorath <jared@xoorath.com>
2016-01-13 13:15:21 -0500

    better layout selection
    still a little rough, but the layout selection now loops over cats
    instead of copy paste. Still need to get colors from the stylus to
    remove the duplication.

--- public/static/js/layout.js
+++ public/static/js/layout.js
@@ -1,35 +1,21 @@
 $(document).ready(function(){
-	
-	var light = ['#EDF393', '#F5E665', '#FFC472', '#FFA891', '#89BABE']
-	var dark = ['#dee937', '#e5cf0f', '#ff990c', '#ff572b', '#508c91'];
-	
-	$('.cat0, .cat0ln').mouseover(function(){
-		$('.cat0').css('background-color', dark[0]);
-	}).mouseleave(function(){
-		$('.cat0').css('background-color', light[0]);
-	});
+  
+  var light = ['#EDF393', '#F5E665', '#FFC472', '#FFA891', '#89BABE']
+  var dark = ['#dee937', '#e5cf0f', '#ff990c', '#ff572b', '#508c91'];
+  
+  for(var i = 0; i < 5; ++i) {
+    var cat = '.cat'+i;
+    var catln = '.cat'+i+'ln';
+    var $cat = $(cat + ', ' + catln);
 
-	$('.cat1, .cat1ln').mouseover(function(){
-		$('.cat1').css('background-color', dark[1]);
-	}).mouseleave(function(){
-		$('.cat1').css('background-color', light[1]);
-	});
+    $cat.data('inner', cat);
+    $cat.data('over', dark[i]);
+    $cat.data('off', light[i]);
 
-	$('.cat2, .cat2ln').mouseover(function(){
-		$('.cat2').css('background-color', dark[2]);
-	}).mouseleave(function(){
-		$('.cat2').css('background-color', light[2]);
-	});
-
-	$('.cat3, .cat3ln').mouseover(function(){
-		$('.cat3').css('background-color', dark[3]);
-	}).mouseleave(function(){
-		$('.cat3').css('background-color', light[3]);
-	});
-
-	$('.cat4, .cat4ln').mouseover(function(){
-		$('.cat4').css('background-color', dark[4]);
-	}).mouseleave(function(){
-		$('.cat4').css('background-color', light[4]);
-	});
+    $cat.mouseover(function(){
+      $($(this).data('inner')).css('background-color', $(this).data('over'));
+    }).mouseleave(function(){
+      $($(this).data('inner')).css('background-color', $(this).data('off'));
+    });
+  }
 });
